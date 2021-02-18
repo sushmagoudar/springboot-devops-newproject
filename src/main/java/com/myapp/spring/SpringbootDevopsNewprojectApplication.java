ac97e484e71eedcfbfac364144d9476ae68b8feb
sushmagoudar <goudarsushma11@gmail.com>
2021-02-18 23:52:35 +0530

    first commit

---
+++ src/main/java/com/myapp/spring/SpringbootDevopsNewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsNewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevopsNewprojectApplication.class, args);
+	}
+
+}
